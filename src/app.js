cfb8854ff7a528fbd7a0b301133a8609f95ccc76
roxcanamat-glitch <roxcanamat@gmail.com>
2026-05-27 01:17:09 +0200

    add root route

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// 👇 RUTA RAÍZ (NUEVA)
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Stress API funcionando 🚀'
+  });
+});
+
 // AUTH
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
