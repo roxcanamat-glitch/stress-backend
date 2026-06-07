5d25b21b537a760981987e28fe9abeb6133c35f7
roxcanamat-glitch <roxcanamat@gmail.com>
2026-06-07 12:31:33 +0200

    fix mood validator

--- src/validators/moodValidator.js
+++ src/validators/moodValidator.js
@@ -5,13 +5,13 @@ const validateMood = (data) => {
   const estresNum = Number(data.estres);
   const { estadoAnimo } = data;
 
-  if (isNaN(ansiedadNum) || ansiedadNum < 0 || ansiedadNum > 10) {
-    return "Ansiedad debe estar entre 0 y 10";
+  if (isNaN(ansiedadNum) || ansiedadNum < 0 || ansiedadNum > 24) {
+    return "Ansiedad debe estar entre 0 y 24";
   }
 
-  if (isNaN(estresNum) || estresNum < 0 || estresNum > 10) {
-    return "Estrés debe estar entre 0 y 10";
-  }
+  // if (isNaN(estresNum) || estresNum < 0 || estresNum > 24) {
+  //   return "Estrés debe estar entre 0 y 24";
+  // }
 
   if (!estadosValidos.includes(estadoAnimo)) {
     return "Estado de ánimo no válido";
